e2829b3ffe6731dd3d2825ae69fc5d51fc88cd40
Ta Duy Duc <duc@ducdigital.com>
2015-12-29 15:42:33 +0100

    Add factory() feature to allow create new instance of money.js

--- tests/index.js
+++ tests/index.js
@@ -1,8 +1,9 @@
 var fx = require('../money.js');
+var util = require('util');
+var chai = require('chai');
 
-var chai = require('chai'),
-expect = chai.expect,
-should = chai.should();
+var expect = chai.expect;
+var should = chai.should();
 
 describe('Main test', function () {
 	fx.base = 'USD';
@@ -54,6 +55,55 @@ describe('Main test', function () {
 
 		it('should be correctly convert with to & from currency chained', function () {
 			fx(100).from('EUR').to('USD').should.be.equal(238.0952380952381);
+		});
+	});
+
+	describe('Multiple instance of fx()', function () {
+		var money = fx.factory();
+
+    money.rates = util._extend({}, fx.rates);
+    money.base = "" + fx.base;
+    money.settings = util._extend({}, fx.settings);
+		money.rates.EUR = 0.5;
+		money.rates.GBP = 0.6;
+
+    it('shoud be a same instance', function () {
+      var copyMoney = require('../money.js');
+      copyMoney.should.be.equal(fx);
+    });
+
+    it('should be a new instance if called with factory()', function () {
+      fx.factory().should.not.be.equal(fx);
+    })
+
+		it('should be correctly convert without currencies', function () {
+			fx.convert(100).should.be.equal(42);
+			money.convert(100).should.be.equal(50);
+		});
+
+		it('should be correctly convert with from currency on second argument', function () {
+			fx.convert(100, {from: 'GBP'}).should.be.equal(182.6086956521739);
+  		money.convert(100, {from: 'GBP'}).should.be.equal(83.33333333333334);
+		});
+
+		it('should be correctly convert with to currency on second argument', function () {
+			fx.convert(100, {to: 'GBP'}).should.be.equal(23);
+      money.convert(100, {to: 'GBP'}).should.be.equal(60);
+		});
+
+		it('should be correctly convert with to & from currency on second argument', function () {
+			fx.convert(100, {from: 'EUR', to: 'USD'}).should.be.equal(238.0952380952381);
+      money.convert(100, {from: 'EUR', to: 'USD'}).should.be.equal(200);
+		});
+
+		it('should be correctly convert with to currency chained', function () {
+			fx(100).to('GBP').should.be.equal(23);
+      money(100).to('GBP').should.be.equal(60);
+		});
+
+		it('should be correctly convert with to & from currency chained', function () {
+			fx(100).from('EUR').to('USD').should.be.equal(238.0952380952381);
+      money(100).from('EUR').to('USD').should.be.equal(200);
 		});
 	});
 
